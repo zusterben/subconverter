b90e63b5069a6090a61496e76f506c78b5df04f3
asdlokj1qpi23 <xdfasdij@qq.com>
2024-03-12 10:36:33 +0800

    fix string_icase_map.(#10)

--- src/utils/map_extra.h
+++ src/utils/map_extra.h
@@ -5,11 +5,9 @@
 #include <map>
 #include <string.h>
 
-struct strICaseComp
-{
-    bool operator() (const std::string &lhs, const std::string &rhs) const
-    {
-        return strcasecmp(lhs.c_str(), rhs.c_str());
+struct strICaseComp {
+    bool operator()(const std::string &lhs, const std::string &rhs) const {
+        return strcasecmp(lhs.c_str(), rhs.c_str()) > 0;
     }
 };
 
